bb914e9d62f292b238a700e2997bea4111c881e4
FuliniF <109399083+FuliniF@users.noreply.github.com>
2025-08-06 14:31:48 +0800

    create env file

--- backend/client.py
+++ backend/client.py
@@ -1,18 +1,24 @@
 import os
+
 import openai
-from supabase import create_client, Client
+from dotenv import load_dotenv
 from pydantic import BaseModel
-from keys import SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY
+from supabase import Client, create_client
+
+load_dotenv()
+
 
 class damy_format(BaseModel):
     cat_name: str
     cat_sex: str
 
+
 class LLM:
     def __init__(self, model="gpt-4.1-nano") -> None:
-        self.client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY_CGI"))
+        OPENAI_API_KEY_CGI = os.getenv("OPENAI_API_KEY_CGI")
+        self.client = openai.OpenAI(OPENAI_API_KEY_CGI)
         self.model = model
-    
+
     def chat(self, message, temperature=0.0, max_tokens=1000, text_format=None):
         if text_format is not None:
             response = self.client.responses.parse(
@@ -33,8 +39,11 @@ class LLM:
             res_text = response.output[0].content[0].text
         return res_text
 
+
 class SupabaseClient:
     def __init__(self) -> None:
+        SUPABASE_URL = os.getenv("SUPABASE_URL")
+        SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
         self.supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
     def get_client(self):
@@ -47,10 +56,13 @@ def test():
     response = llm.chat(message)
     print(response)
     print("------")
-    message = [{"role": "user", "content": "give me one name for a cat and specify its sex."}]
+    message = [
+        {"role": "user", "content": "give me one name for a cat and specify its sex."}
+    ]
     llm = LLM()
     response = llm.chat(message, text_format=damy_format)
     print(response)
+
 
 if __name__ == "__main__":
     test()
